bd58970407b386306fdab2a4fe45780688b72471
memonsalman <memonsalman@users.noreply.github.com>
2021-01-11 15:20:34 +0530

    Add files via upload

---
+++ Templates/Alert.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html style="margin: 0;padding: 0;" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title></title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width" />
+
+
+	<style type="text/css">
+		.body{
+			padding: 20px 50px;
+		}
+		.maindiv{
+			padding: 20px 30px;
+		}
+
+		@media only screen and (max-width:620px){
+			.body{
+				padding: 20px 10px !important;
+			}
+			.maindiv{
+				padding: 20px 10px!important;
+			}
+		}
+	</style>
+
+</head>
+<body style="margin:0;padding: 0;">
+	<table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+		<tbody>
+			<tr>
+				<td>
+					<div style="padding: 20px 30px;background: #f2f3f5;" class="maindiv">
+						<div class="head" style="width: 100%;height: 50px;background: #2196F3;border-radius: 30px 30px 0 0;">
+						</div>
+						<div class="body" style="background: white;padding: 20px 50px;">
+							<div class="section">
+								<div class="comp">
+									<div class="compimg" style="width: 100px;height: auto;margin: 0 auto;">
+										<img src="--COMPANY_LOGO--" style="width: 100%;height: 100%;">
+									</div>
+									<div class="compname" style="text-align: center;font-size: 35px;">--COMPANY_NAME--</div>
+								</div>
+								<div class="content" style="padding: 20px 0;border-top: 1px solid #00000038;border-bottom: 1px solid #00000038;">
+
+									<h1 style="text-align: center">[My Sunless - Security Alert]</h1>
+									<p>Dear --FIRSTNAME-- --LASTNAME-- ( --USERNAME-- ),
+										<br>
+										Our system detected that your account was logged in by a different system @ <span style="color:red;">--LOGINTIME--</span>.
+										<br>
+										Your account is at risk if this wasn't you.
+										<br>
+										Do you recognise your activity? 
+									</p>
+									
+									<table>
+										<tbody><tr>
+											<td>
+												<a style="text-decoration: none;background: #26ab26;padding: 10px;color:
+												white;" target="_blank" href="https://mysunless.com/crm/allcamscriptcron.php?AccessLoginYes=--TOKENID--">Yes, it was me</a></td>
+												<td>
+													<a target="_blank" style="text-decoration: none;background: red;padding: 10px;color:
+													white;" href="https://mysunless.com/crm/allcamscriptcron.php?AccessLoginNo=--TOKENID--">No, it wasn't me.</a>
+												</td>
+											</tr>
+										</tbody></table>
+										<br>
+									<small>Save your Request ID: <span style="color: #26ab26">--TOKENID--</span> for future purpose.</small>
+										<br>
+										Thanks
+										<br>
+										Mysunless Team
+
+
+									</div>
+
+									<div class="footer" style="padding: 10px;margin-top: 10px;">
+										<table align="center" border="0" cellpadding="0" cellspacing="0" class="social_media">
+											<tr>
+												--social_icon--
+											</tr>
+										</table>                                                         
+									</div>
+									<div class="copyright" style="text-align: center;">
+										Copyright © *2020* *MySunless*, All rights reserved.
+									</div>
+								</div>
+							</div>
+							<div class="bottom" style="width: 100%;height: 50px;background: #2196F3;border-radius: 0 0 30px 30px;"></div>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table> 
+	</body>
+	</html>
